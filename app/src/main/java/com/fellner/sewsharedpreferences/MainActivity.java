df85f19eac139cafe8d3fdd66118037ac41dc273
Manuel Fellner <mfellner@student.tgm.ac.at>
2023-09-24 16:17:31 +0200

    feat(mainActivity): name can be stored, shared preferences can be read at startup

--- app/src/main/java/com/fellner/sewsharedpreferences/MainActivity.java
+++ app/src/main/java/com/fellner/sewsharedpreferences/MainActivity.java
@@ -2,13 +2,74 @@ package com.fellner.sewsharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText nameTextInput = (EditText) findViewById(R.id.textfield_name);
+
+        String currentSharedPreferenceNameValue = returnNameOutOfSharedPreferences();
+
+        // if the current shared preferences have the name already saved
+        if (!currentSharedPreferenceNameValue.equals(""))    {
+            nameTextInput.setText(currentSharedPreferenceNameValue);
+        } else {
+            String initialValue = getResources().getString(R.string.initial_value);
+            nameTextInput.setText(initialValue);
+        }
+
+        // get the save button view
+        final Button saveButton = (Button) findViewById(R.id.button_save);
+        //attach an eventlistener to the save button
+        saveButton.setOnClickListener(new View.OnClickListener()  {
+            public void onClick (View v)    {
+                String userNameInput = nameTextInput.getText().toString();
+                storeInputIntoSharedPreferences(userNameInput);
+                Log.i(LOG_TAG,"Successfully saved \"" + userNameInput + "\" into the shared preferences!");
+            }
+        });
+
+        // get the delete button view
+        final Button deleteButton = (Button) findViewById(R.id.button_delete);
+        //attach an eventlistener to the delete button
+        deleteButton.setOnClickListener(new View.OnClickListener()  {
+            public void onClick (View v)    {
+
+            }
+        });
+    }
+
+    private void storeInputIntoSharedPreferences(String inputToStore)  {
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
+
+
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        // put our input in the editor
+        edit.putString("name", inputToStore);
+        //commit the changes -> they'll get saved
+        edit.commit();
+    }
+
+
+    private String returnNameOutOfSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
+
+        return sharedPreferences.getString("name", "");
     }
 }
